ccb6e7b3df44e9872589da5ca46e8313b566012b
Nick Garfield <nick@and.xyz>
2022-02-07 13:27:20 -0700

    Remove indexer program

--- programs/indexer/client/src/types.ts
+++
@@ -1,6 +0,0 @@
-import { AccountData } from "@cronos-so/utils";
-import { Indexer } from "./idl";
-
-export type ElementAccountData = AccountData<Indexer["accounts"][0], Indexer>;
-
-export type ListAccountData = AccountData<Indexer["accounts"][1], Indexer>;
--- programs/indexer/src/instructions/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod create_list;
-pub mod delete_list;
-pub mod pop_element;
-pub mod push_element;
-
-pub use create_list::*;
-pub use delete_list::*;
-pub use pop_element::*;
-pub use push_element::*;
