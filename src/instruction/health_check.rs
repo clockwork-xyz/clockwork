d22450ae198080e5dd6ae169ab9a9413b2f48c5c
Nick Garfield <nick@and.xyz>
2022-02-09 16:24:42 -0700

    Update health_check instruction

--- src/instruction/health_check.rs
+++ src/instruction/health_check.rs
@@ -7,12 +7,18 @@ use anchor_client::anchor_lang::{
     InstructionData,
 };
 
-pub fn health_check(authority: Pubkey, daemon: Pubkey, health: Pubkey) -> Instruction {
+pub fn health_check(
+    authority: Pubkey,
+    config: Pubkey,
+    daemon: Pubkey,
+    health: Pubkey,
+) -> Instruction {
     Instruction {
         program_id: cronos_program::ID,
         accounts: vec![
-            AccountMeta::new_readonly(sysvar::clock::ID, false),
             AccountMeta::new_readonly(authority, false),
+            AccountMeta::new_readonly(sysvar::clock::ID, false),
+            AccountMeta::new_readonly(config, false),
             AccountMeta::new(daemon, true),
             AccountMeta::new(health, false),
         ],
