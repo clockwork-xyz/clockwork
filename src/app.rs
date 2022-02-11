286a2ad145e430581c81475c1a457d0878d661e8
Nick Garfield <nick@and.xyz>
2022-02-11 11:41:26 -0800

    Cleanup command line parsing

---
+++ src/app.rs
@@ -0,0 +1,35 @@
+use clap::{App, AppSettings, Arg};
+
+pub fn cronos_app() -> App<'static> {
+    App::new("Cronos")
+        .bin_name("cronos")
+        .about("Cronos is an instruction scheduler for Solana")
+        .version("0.0.1")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(daemon_app())
+        .subcommand(task_app())
+}
+
+fn daemon_app() -> App<'static> {
+    App::new("daemon")
+        .about("Manage your daemon")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(App::new("data").about("Fetch daemon account data"))
+        .subcommand(App::new("new").about("Create a daemon account"))
+}
+
+fn task_app() -> App<'static> {
+    App::new("task")
+        .about("Manage your tasks")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("data").about("Fetch task account data").arg(
+                Arg::new("address")
+                    .index(1)
+                    .takes_value(true)
+                    .required(true)
+                    .help("A task address"),
+            ),
+        )
+        .subcommand(App::new("new").about("Create a task"))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,12 @@
+pub mod app;
 pub mod command;
 pub mod error;
 
+use crate::app::cronos_app;
 use crate::command::CliCommand;
 
-use clap::{App, AppSettings, Arg};
-
 fn main() {
-    let matches = App::new("Cronos")
-        .bin_name("cronos")
-        .about("Cronos is an instruction scheduler for Solana")
-        .version("0.0.1")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(daemon_app())
-        .subcommand(task_app())
-        .get_matches();
-
+    let matches = cronos_app().get_matches();
     let command = CliCommand::try_from(&matches);
-
     println!("The command: {:?}", command);
-}
-
-fn daemon_app() -> App<'static> {
-    App::new("daemon")
-        .about("Manage your daemon")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(App::new("data").about("Fetch daemon account data"))
-        .subcommand(App::new("new").about("Create a daemon account"))
-}
-
-fn task_app() -> App<'static> {
-    App::new("task")
-        .about("Manage your tasks")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(
-            App::new("data").about("Fetch task account data").arg(
-                Arg::new("address")
-                    .index(1)
-                    .takes_value(true)
-                    .required(true)
-                    .help("A task address"),
-            ),
-        )
-        .subcommand(App::new("new").about("Create a task"))
 }
