cc9de107c5217be0cfdc4801dd37de9947f2d336
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-02-17 16:58:09 -0500

    fix web submodule

--- web/src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-import { BN } from "@project-serum/anchor";
-
-export function dateToSeconds(date: Date): number {
-  return Math.floor(date.getTime() / 1000);
-}
-
-export function now(): BN {
-  return new BN(dateToSeconds(new Date()));
-}
--- web/src/utils/sleep.ts
+++
@@ -1,9 +0,0 @@
-export async function sleep(time: number) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-export async function sleepUntil(date: Date) {
-  const now = new Date();
-  const duration = date.getTime() - now.getTime();
-  await sleep(duration);
-}
