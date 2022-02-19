172655a23fa799034484fff6f73bc12b7e5bf9f7
Nick Garfield <nick@and.xyz>
2022-02-19 08:59:11 -0700

    Implement daemon_widthdraw helper

---
+++ src/instruction/daemon_widthdraw.rs
@@ -0,0 +1,19 @@
+use anchor_client::anchor_lang::{
+    solana_program::{
+        instruction::{AccountMeta, Instruction},
+        pubkey::Pubkey,
+    },
+    InstructionData,
+};
+use cronos_program::state::InstructionData as CronosInstructionData;
+
+pub fn daemon_widthdraw(daemon: Pubkey, owner: Pubkey, amount: u64) -> Instruction {
+    Instruction {
+        program_id: cronos_program::ID,
+        accounts: vec![
+            AccountMeta::new(daemon, false),
+            AccountMeta::new(owner, true),
+        ],
+        data: cronos_program::instruction::DaemonWidthdraw { amount }.data(),
+    }
+}
