9d4e2d599cc99fd17fe4c2434c40047c73b0ec5a
Nick Garfield <nick@and.xyz>
2022-02-22 15:53:32 -0700

    Bump to 0.1.2

--- src/utils/new_rpc_client.rs
+++ src/utils/new_rpc_client.rs
@@ -7,7 +7,7 @@ use {
 
 pub fn new_rpc_client() -> Client {
     let payer = read_keypair(&mut File::open(env::keypath().as_str()).unwrap()).unwrap();
-    let client = RpcClient::new_with_commitment(
+    let client = RpcClient::new_with_commitment::<String>(
         env::rpc_endpoint().as_str().into(),
         CommitmentConfig::confirmed(),
     );
