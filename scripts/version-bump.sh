4696101ce4b5ffd5bac7f08041a22eb01fa7dbef
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-02-24 19:41:56 -0700

    basic version bump script

---
+++ scripts/version-bump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+declare -a arr=("programs" "sdk" "cli" "bot")
+declare -a paths=(../programs/cronos/Cargo.toml ../cli/Cargo.toml ../bot/Cargo.toml ../sdk/Cargo.toml)
+
+for(( c = 0 ; c < 4 ; c++))
+do
+  echo ${arr[$c]} version bump:
+  read bump_version
+  sed -i -e '3s/.*/'${bump_version}'/' ${paths[$c]}
+done
