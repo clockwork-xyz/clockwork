fd5daf00ab859a259cfe1b56f7bddb9cbd54993c
Nick Garfield <nick@and.xyz>
2022-02-25 12:59:18 -0700

    Bump to 0.1.4

--- version_bump.sh
+++ version_bump.sh
@@ -6,7 +6,7 @@ echo "Bumping from $(cat VERSION) to ${new_version}"
 old_version=$(cat VERSION)
 
 # Build
-RUSTFLAGS="--deny warnings" cargo build || (echo "❌  Build failed!" && exit)
+RUSTFLAGS="--deny warnings" cargo build || (echo "Build failed" && exit)
 
 # Bump cronos-program 
 sed -i '' -e '3s/^version = "'${old_version}'"/version = "'${new_version}'"/g' programs/cronos/Cargo.toml
@@ -23,20 +23,17 @@ sed -i '' -e '3s/^version = "'${old_version}'"/version = "'${new_version}'"/g' b
 sed -i '' -e 's/^cronos-sdk =.*/cronos-sdk = { path = "..\/sdk", version = "'${new_version}'" }/g' cli/Cargo.toml
 sed -i '' -e '3s/^version = "'${old_version}'"/version = "'${new_version}'"/g' cli/Cargo.toml
 
+# Cargo publish
+cargo publish --allow-dirty --manifest-path programs/cronos/Cargo.toml || (echo "Failed to publish cronos-program" && exit)
+cargo publish --allow-dirty --manifest-path sdk/Cargo.toml || (echo "Failed to publish cronos-sdk" && exit)
+cargo publish --allow-dirty --manifest-path bot/Cargo.toml || (echo "Failed to publish cronos-bot" && exit)
+cargo publish --allow-dirty --manifest-path cli/Cargo.toml || (echo "Failed to publish cronos-cli" && exit)
+
 # Update version
 echo $new_version > VERSION
-echo "$(git diff --stat | tail -n1)"
-
-# Cargo publish
-cargo publish --allow-dirty --manifest-path programs/cronos/Cargo.toml
-cargo publish --allow-dirty --manifest-path sdk/Cargo.toml
-cargo publish --allow-dirty --manifest-path bot/Cargo.toml
-cargo publish --allow-dirty --manifest-path cli/Cargo.toml
-
-# Re-build
-cargo build
 
 # Git commit 
+echo "$(git diff --stat | tail -n1)"
 git checkout -b release/${new_version}
 git add .
 git commit -m "Bump to $new_version"
