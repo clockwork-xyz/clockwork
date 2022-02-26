8758ac3512054e6f3e6b915b0795bf9bd3260011
Nick Garfield <nick@and.xyz>
2022-02-26 07:54:19 -0700

    Update version script

--- dev/version.sh
+++ dev/version.sh
@@ -10,21 +10,21 @@ echo "Bumping from $current_version to $new_version"
 RUSTFLAGS="--deny warnings" cargo build || (echo "Build failed" && exit)
 
 # Bump cronos-program 
-path=programs/cronos/Cargo.toml
+path=../programs/cronos/Cargo.toml
 sed -i '' -e '3s/^version = "'${current_version}'"/version = "'${new_version}'"/g' path
 
 # Bump cronos-sdk
-path=sdk/Cargo.toml
+path=../sdk/Cargo.toml
 sed -i '' -e 's/^cronos-program =.*/cronos-program = { path = "..\/programs\/cronos", features = ["no-entrypoint"], version = "'${new_version}'" }/g' path
 sed -i '' -e '3s/^version = "'${current_version}'"/version = "'${new_version}'"/g' path
 
 # Bump cronos-bot
-path=bot/Cargo.toml
+path=../bot/Cargo.toml
 sed -i '' -e 's/^cronos-sdk =.*/cronos-sdk = { path = "..\/sdk", version = "'${new_version}'" }/g' path
 sed -i '' -e '3s/^version = "'${current_version}'"/version = "'${new_version}'"/g' path
 
 # Bump cronos-cli
-path=cli/Cargo.toml
+path=../cli/Cargo.toml
 sed -i '' -e 's/^cronos-sdk =.*/cronos-sdk = { path = "..\/sdk", version = "'${new_version}'" }/g' path
 sed -i '' -e '3s/^version = "'${current_version}'"/version = "'${new_version}'"/g' path
 
@@ -34,7 +34,7 @@ echo $new_version > VERSION
 # Git commit 
 echo "$(git diff --stat | tail -n1)"
 git checkout -b release/${new_version}
-git add .
+git add ..
 git commit -m "Bump to $new_version"
 git tag "v$new_version"
 git push --set-upstream origin release/${new_version} --tags
