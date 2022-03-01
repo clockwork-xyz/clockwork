2d5a03bec4e52f8d37e65582ea1b633a6305e3d6
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-03-01 15:45:55 -0700

    subscribing to sysvar clock instead then calling health account

--- monitor/src/env.rs
+++ monitor/src/env.rs
@@ -1,5 +1,13 @@
 use std::env;
 
+pub fn keypath() -> String {
+    env::var("KEYPATH").unwrap()
+}
+
 pub fn wss_endpoint() -> String {
     env::var("WSS_ENDPOINT").unwrap()
+}
+
+pub fn rpc_endpoint() -> String {
+    env::var("RPC_ENDPOINT").unwrap()
 }
--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,21 +1,31 @@
+use solana_sdk::clock::*;
+
 use {
     cronos_sdk::account::Health,
     dotenv::dotenv,
     solana_account_decoder::UiAccountEncoding,
     solana_client::{pubsub_client::PubsubClient, rpc_config::RpcAccountInfoConfig},
-    solana_sdk::{account::Account, commitment_config::CommitmentConfig},
+    solana_sdk::{account::Account, commitment_config::CommitmentConfig, pubkey::Pubkey},
+    std::str::FromStr,
 };
 
 mod env;
+mod new_rpc_client;
+
+use crate::new_rpc_client::new_rpc_client;
 
 fn main() {
     dotenv().ok();
+    const FIVE_MINUTES: i32 = 300;
+
+    let client = new_rpc_client();
 
     let (health_pubkey, _health_bump) = Health::pda();
+    let clock_addr = Pubkey::from_str("SysvarC1ock11111111111111111111111111111111").unwrap();
 
-    let (_ws_client, health_receiver) = PubsubClient::account_subscribe(
+    let (_ws_client, clock_receiver) = PubsubClient::account_subscribe(
         env::wss_endpoint().as_str().into(),
-        &health_pubkey,
+        &clock_addr,
         Some(RpcAccountInfoConfig {
             encoding: Some(UiAccountEncoding::Base64),
             commitment: Some(CommitmentConfig::processed()),
@@ -24,12 +34,36 @@ fn main() {
     )
     .unwrap();
 
-    for ui_account_response in health_receiver {
+    for ui_account_response in clock_receiver {
         let ui_account = ui_account_response.value;
         let account = ui_account.decode::<Account>().unwrap();
-        let health = Health::try_from(account.data).unwrap();
+        let clock = get_clock_from_data(account.data);
 
-        println!("last ping: {}", health.last_ping);
-        println!("target ping: {}\n", health.target_ping);
+        let health_account = client.get_account_data(&health_pubkey).unwrap();
+        let health_data = Health::try_from(health_account).unwrap();
+
+        let blocktime = clock.unix_timestamp;
+        let last_ping = blocktime - health_data.last_ping;
+        let recurr_drift = blocktime - health_data.target_ping;
+
+        println!("   Blocktime: {}", blocktime);
+        println!("   Last ping: {} sec", last_ping);
+        println!("Recurr drift: {} sec\n", recurr_drift);
+
+        if recurr_drift > FIVE_MINUTES.into() {
+            //TODO: CALL TWILLIO API
+        }
+    }
+}
+
+fn get_clock_from_data(data: Vec<u8>) -> Clock {
+    Clock {
+        slot: Slot::from_le_bytes(data.as_slice()[0..8].try_into().unwrap()),
+        epoch_start_timestamp: UnixTimestamp::from_le_bytes(
+            data.as_slice()[8..16].try_into().unwrap(),
+        ),
+        epoch: Epoch::from_le_bytes(data.as_slice()[16..24].try_into().unwrap()),
+        leader_schedule_epoch: Epoch::from_le_bytes(data.as_slice()[24..32].try_into().unwrap()),
+        unix_timestamp: UnixTimestamp::from_le_bytes(data.as_slice()[32..40].try_into().unwrap()),
     }
 }
