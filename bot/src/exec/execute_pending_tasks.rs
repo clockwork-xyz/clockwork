29867cc5220d3801176f0d24d1b4d6733eb295b8
Nick Garfield <nick@and.xyz>
2022-03-01 08:07:52 -0700

    Add const for lookback window

--- bot/src/exec/execute_pending_tasks.rs
+++ bot/src/exec/execute_pending_tasks.rs
@@ -4,8 +4,10 @@ use std::sync::{Arc, RwLock};
 use crate::execute_task;
 use crate::store::TaskStore;
 
+const LOOKBACK_WINDOW: i64 = 10; // Number of seconds to lookback
+
 pub fn execute_pending_tasks(client: Arc<Client>, store: Arc<RwLock<TaskStore>>, blocktime: i64) {
-    for t in (blocktime - 10)..blocktime {
+    for t in (blocktime - LOOKBACK_WINDOW)..blocktime {
         let r_store = store.read().unwrap();
         r_store.index.get(&t).and_then(|keys| {
             for key in keys.iter() {
