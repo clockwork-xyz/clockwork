55a694e7d54bdd69edd1d0357d5e6afb091b121f
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-03-10 12:05:24 -0600

    indexing data to es instance

--- telemetry/src/env.rs
+++ telemetry/src/env.rs
@@ -4,6 +4,26 @@ pub fn keypath() -> String {
     env::var("KEYPATH").unwrap()
 }
 
+pub fn wss_endpoint() -> String {
+    env::var("WSS_ENDPOINT").unwrap()
+}
+
 pub fn rpc_endpoint() -> String {
     env::var("RPC_ENDPOINT").unwrap()
+}
+
+pub fn es_cloud_id() -> String {
+    env::var("es_cloud_id").unwrap()
+}
+
+pub fn es_user() -> String {
+    env::var("es_user").unwrap()
+}
+
+pub fn es_password() -> String {
+    env::var("es_password").unwrap()
+}
+
+pub fn es_health_index() -> String {
+    env::var("es_health_index").unwrap()
 }
--- telemetry/src/main.rs
+++ telemetry/src/main.rs
@@ -1,30 +1,68 @@
 use {
+    chrono::{TimeZone, Utc},
     cronos_sdk::account::Health,
     dotenv::dotenv,
+    elasticsearch::IndexParts,
+    elasticsearch::{auth::Credentials, http::transport::Transport, Elasticsearch, Error},
+    env::wss_endpoint,
+    serde_json::json,
     solana_client_helpers::Client,
     solana_client_helpers::RpcClient,
     solana_sdk::{commitment_config::CommitmentConfig, signature::read_keypair},
-    std::{fs::File, sync::Arc},
+    std::{fs::File, result::Result, sync::Arc},
 };
 
 mod env;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+
+    let mut latest_ts: i64 = 0;
+
+    let es_client = elastic_client().unwrap();
     let client = &Arc::new(new_client());
-    let (health_pubkey, _health_bump) = Health::pda();
-    let time_receiver = cronos_sdk::clock::monitor_time(client);
+
+    let health_pubkey = Health::pda().0;
+    let time_receiver = cronos_sdk::clock::monitor_time(wss_endpoint().as_str().into());
+
     for ts in time_receiver {
-        let health_account = client.get_account_data(&health_pubkey).unwrap();
-        let health_data = Health::try_from(health_account).unwrap();
+        if ts > latest_ts + 9 {
+            latest_ts = ts;
 
-        let last_ping = ts - health_data.last_ping;
-        let recurr_drift = ts - health_data.target_ping;
+            let health_account = client.get_account_data(&health_pubkey).unwrap();
+            let health_data = Health::try_from(health_account).unwrap();
 
-        println!("       Clock: {}", ts);
-        println!("   Last ping: {} sec", last_ping);
-        println!("Recurr drift: {} sec\n", recurr_drift);
+            let last_ping = latest_ts - health_data.last_ping;
+            let recurr_drift = latest_ts - health_data.target_ping;
+
+            let dt = Utc.timestamp(ts, 0);
+            let ts_utc = dt.naive_utc();
+
+            println!("   Clock UTC: {}", ts_utc);
+            println!("       Clock: {}", ts);
+            println!("   Last ping: {} sec", last_ping);
+            println!("Recurr drift: {} sec", recurr_drift);
+
+            let response = es_client
+                .index(IndexParts::IndexId(
+                    env::es_health_index().as_str(),
+                    &ts.to_string(),
+                ))
+                .body(json!({
+                    "blocktime": ts_utc,
+                    "last_ping": last_ping,
+                    "drift": recurr_drift
+                }))
+                .send()
+                .await?;
+
+            let successful = response.status_code().is_success();
+
+            println!("  successful: {}\n", successful);
+        }
     }
+    Ok(())
 }
 
 fn new_client() -> Client {
@@ -34,4 +72,13 @@ fn new_client() -> Client {
         CommitmentConfig::processed(),
     );
     Client { client, payer }
+}
+
+fn elastic_client() -> Result<Elasticsearch, Error> {
+    let cloud_id = env::es_cloud_id();
+    let credentials = Credentials::Basic(env::es_user(), env::es_password());
+    let transport = Transport::cloud(&cloud_id, credentials)?;
+    let client = Elasticsearch::new(transport);
+
+    Ok(client)
 }
