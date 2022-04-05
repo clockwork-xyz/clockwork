5eaf4e7c2a240e0acd83886df4fd63d2756c4529
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-04-05 15:17:00 -0500

    deleting to get ready for merge

--- ci/solana-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Prints the Solana version.
-
-set -e
-
-cd "$(dirname "$0")/.."
-
-cargo read-manifest | jq -r '.dependencies[] | select(.name == "solana-accountsdb-plugin-interface") | .req'
