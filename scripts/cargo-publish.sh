97002401ec9e4a10bf2e32b60ca6d32581f0f38f
nickgarfield <ntgarfield@gmail.com>
2022-04-12 23:29:57 -0500

    Quick edits

--- scripts/cargo-publish.sh
+++ scripts/cargo-publish.sh
@@ -3,7 +3,7 @@
 cargo publish -p cronos-cron
 sleep 25
 cargo publish -p cronos-heartbeat
-cargo publish -p cronos-pool
+cargo publish -p cronos-network
 cargo publish -p cronos-scheduler
 sleep 25
 cargo publish -p cronos-sdk
