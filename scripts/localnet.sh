1e241eb301a8a1813ee496948fd9dff876666865
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-04-25 11:30:07 -0500

    pool -> network

--- scripts/localnet.sh
+++ scripts/localnet.sh
@@ -2,6 +2,6 @@
 
 solana-test-validator -r \
     --bpf-program target/deploy/cronos_heartbeat-keypair.json target/deploy/cronos_heartbeat.so \
-    --bpf-program target/deploy/cronos_pool-keypair.json target/deploy/cronos_pool.so \
+    --bpf-program target/deploy/cronos_network-keypair.json target/deploy/cronos_network.so \
     --bpf-program target/deploy/cronos_scheduler-keypair.json target/deploy/cronos_scheduler.so \
     --geyser-plugin-config plugin/config.json
