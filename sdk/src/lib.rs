b52561a33e20392be365b62bd9fd7b1b802fbe68
Nick Garfield <nick@and.xyz>
2022-05-04 14:16:55 -0500

    Cleanup sdk files

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,4 +1,3 @@
-// pub mod clock;
 pub mod heartbeat;
 pub mod network;
 pub mod scheduler;
@@ -6,10 +5,6 @@ pub mod scheduler;
 pub use cronos_scheduler::errors;
 pub use cronos_scheduler::pda;
 
-// Export current solana-program types for downstream users who may also be
-// building with a different solana-program version
-// pub use anchor_lang::solana_program;
-
-// Program IDs
 pub use cronos_heartbeat::ID as HEARTBEAT_PROGRAM_ID;
+pub use cronos_network::ID as NETWORK_PROGRAM_ID;
 pub use cronos_scheduler::ID as SCHEDULER_PROGRAM_ID;
