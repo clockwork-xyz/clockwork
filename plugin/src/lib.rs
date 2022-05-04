555f7d482828ad5b83bfa1fc73c1611117b89c9e
Nick Garfield <nick@and.xyz>
2022-05-04 09:31:21 -0500

    Remove dotenv from plugin dependencies

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,7 +1,4 @@
-use {
-    dotenv::dotenv,
-    solana_geyser_plugin_interface::geyser_plugin_interface::GeyserPlugin,
-};
+use solana_geyser_plugin_interface::geyser_plugin_interface::GeyserPlugin;
 
 mod bucket;
 mod cache;
@@ -21,9 +18,6 @@ pub use {bucket::Bucket, cache::TaskCache, config::Config, filter::Filter, plugi
 /// The Solana validator and this plugin must be compiled with the same Rust compiler version and Solana core version.
 /// Loading this plugin with mismatching versions is undefined behavior and will likely cause memory corruption.
 pub unsafe extern "C" fn _create_plugin() -> *mut dyn GeyserPlugin {
-    // Load env file
-    dotenv().ok();
-
     let plugin = CronosPlugin::new();
     let plugin: Box<dyn GeyserPlugin> = Box::new(plugin);
     Box::into_raw(plugin)
