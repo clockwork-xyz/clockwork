cc444ee4cb0884a2c547597f0fd4b5d9af41259f
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-05-24 16:45:20 -0500

    update solana deps script

--- scripts/update-solana.sh
+++ scripts/update-solana.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 
+# Get new version
+current_version=$(cat ./VERSION)
+echo "Current version: $current_version"
+read -r -p "    New version: " new_version
 
-deps=(
-    solana-account-decoder,
-    solana-clap-utils,
-    solana-cli-config,
-    solana-client,
-    solana-geyser-plugin-interface,
-    solana-logger,
-    solana-program,
-    solana-remote-wallet,
-    solana-sdk
-)
+# Find all Cargo.toml files
+cargo_tomls=($(find . -type f -name "Cargo.toml"))
+
+# Find and replace with new_version
+for cargo_toml in "${cargo_tomls[@]}"; do
+    sed -i '' -e "/^solana-/s/=.*/= \"$new_version\"/g" $cargo_toml
+done
