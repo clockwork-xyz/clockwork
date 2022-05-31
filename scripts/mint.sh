b2c63bd83ae8932efc3722d9fc37e685d8730f97
Nick Garfield <nick@and.xyz>
2022-05-31 14:58:22 -0500

    Bump from 0.2.0-alpha3 to 0.2.0-alpha4

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -13,5 +13,5 @@ balance=1000
 spl-token mint $mint $balance
 
 # Transfer 100 to the validator keypair
-validator_identity=$(solana address -k ./test-ledger/validator-keypair.json)
-spl-token transfer $mint 100 $validator_identity --fund-recipient
+# validator_identity=$(solana address -k ./test-ledger/validator-keypair.json)
+# spl-token transfer $mint 100 $validator_identity --fund-recipient
