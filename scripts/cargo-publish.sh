732c3a48abc15485e6ccf53ca5283d9d923f5f89
Nick Garfield <nick@and.xyz>
2022-08-15 13:38:35 -0600

    Update cargo-publish script with new crates

--- scripts/cargo-publish.sh
+++ scripts/cargo-publish.sh
@@ -2,12 +2,16 @@
 
 cargo publish -p clockwork-cron
 sleep 25
-cargo publish -p clockwork-health
+cargo publish -p clockwork-pool
 sleep 25
 cargo publish -p clockwork-http
-sleep 25
-cargo publish -p clockwork-pool
 sleep 25
 cargo publish -p clockwork-scheduler
 sleep 25
 cargo publish -p clockwork-network
+sleep 25
+cargo publish -p clockwork-health
+sleep 25 
+cargo publish -p clockwork-client
+sleep 25
+cargo publish -p clockwork-cli
