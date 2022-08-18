f5dc2fa71d8f5994ca1c17d071556d375ffa1e88
Nick Garfield <nick@and.xyz>
2022-08-18 09:59:48 -0600

    Rearrange ixs in initialize cli command

--- cli/src/processor/initialize.rs
+++ cli/src/processor/initialize.rs
@@ -3,8 +3,8 @@ use {crate::errors::CliError, clockwork_client::Client, solana_sdk::pubkey::Pubk
 pub fn initialize(client: &Client, mint: Pubkey) -> Result<(), CliError> {
     // Initialize the programs
     let admin = client.payer_pubkey();
-    let ix_a = clockwork_client::http::instruction::initialize(admin);
-    let ix_b = clockwork_client::crank::instruction::initialize(admin);
+    let ix_a = clockwork_client::crank::instruction::initialize(admin);
+    let ix_b = clockwork_client::http::instruction::initialize(admin);
     let ix_c = clockwork_client::network::instruction::initialize(admin, mint);
     let ix_d = clockwork_client::pool::instruction::initialize(admin);
 
