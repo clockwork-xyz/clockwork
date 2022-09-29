b8cc2241f460d77276a87a4b9439e75afc72f2f4
Nick Garfield <nick@and.xyz>
2022-09-29 14:43:38 -0400

    Edits to the SDK

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,65 +1,8 @@
-#[cfg(feature = "crank")]
-pub use clockwork_crank::state::{
-    AccountMetaData, ClockData, Config as CrankConfig, ConfigAccount as CrankConfigAccount,
-    ConfigSettings as CrankConfigSettings, CrankResponse, ExecContext, Fee, FeeAccount,
-    InstructionData, Queue, QueueAccount, Trigger, TriggerContext,
-    SEED_CONFIG as SEED_CRANK_CONFIG, SEED_FEE, SEED_QUEUE,
-};
-
-#[cfg(feature = "http")]
-pub use clockwork_http::state::{
-    Api, ApiAccount, Config as HttpConfig, ConfigAccount as HttpConfigAccount,
-    ConfigSettings as HttpConfigSettings, HttpMethod, Request, RequestAccount, SEED_API,
-    SEED_CONFIG as SEED_HTTP_CONFIG, SEED_REQUEST,
-};
-
-#[cfg(feature = "network")]
-pub use clockwork_network::state::{
-    Authority, Config as NetworkConfig, ConfigAccount as NetworkConfigAccount,
-    ConfigSettings as NetworkConfigSettings, Node, NodeAccount, NodeSettings, Registry,
-    RegistryAccount, Rotator, RotatorAccount, Snapshot, SnapshotAccount, SnapshotEntry,
-    SnapshotEntryAccount, SnapshotStatus, SEED_AUTHORITY, SEED_CONFIG as SEED_NETWORK_CONFIG,
-    SEED_NODE, SEED_REGISTRY, SEED_ROTATOR, SEED_SNAPSHOT, SEED_SNAPSHOT_ENTRY,
-};
-
-#[cfg(feature = "pool")]
-pub use clockwork_pool::state::{
-    Config as PoolConfig, ConfigAccount as PoolConfigAccount, ConfigSettings as PoolConfigSettings,
-    Pool, PoolAccount, PoolSettings, SEED_CONFIG as SEED_POOL_CONFIG, SEED_POOL,
-};
-
-pub mod program {
-    #[cfg(feature = "crank")]
-    pub mod crank {
-        pub use clockwork_crank::{
-            accounts::*, anchor::*, clockwork_crank::*, cpi::*, entry, errors::*, errors::*, id::*,
-            instruction::*, payer::*, program::ClockworkCrank,
-        };
-    }
-    #[cfg(feature = "http")]
-    pub mod http {
-        pub use clockwork_http::{
-            accounts::*, clockwork_http::*, cpi::*, entry, errors::*, errors::*, id::*,
-            instruction::*, program::ClockworkHttp,
-        };
-    }
-    #[cfg(feature = "network")]
-    pub mod network {
-        pub use clockwork_network::{
-            accounts::*, clockwork_network::*, cpi::*, entry, errors::*, errors::*, id::*,
-            instruction::*, program::ClockworkNetwork,
-        };
-    }
-    #[cfg(feature = "pool")]
-    pub mod pool {
-        pub use clockwork_pool::{
-            accounts::*, clockwork_pool::*, cpi::*, entry, errors::*, errors::*, id::*,
-            instruction::*, program::ClockworkPool,
-        };
-    }
-}
+#[cfg(feature = "client")]
+pub use clockwork_client::Client;
 
 #[cfg(feature = "client")]
-pub mod client {
-    pub use clockwork_client::*;
-}
+pub use clockwork_client::crank::*;
+
+#[cfg(feature = "crank")]
+pub use ::clockwork_crank::*;
