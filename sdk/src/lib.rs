ced39048686b73402ecc09d7e6f87a5f8e13c904
Nick Garfield <nick@and.xyz>
2022-09-29 17:03:40 -0400

    Update import scheme of clockwork programs

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -4,5 +4,4 @@ pub use clockwork_client::Client;
 #[cfg(feature = "client")]
 pub use clockwork_client::crank::*;
 
-#[cfg(feature = "crank")]
-pub use ::clockwork_crank::*;
+pub use ::clockwork_crank::{program::ClockworkCrank as Program, *};
