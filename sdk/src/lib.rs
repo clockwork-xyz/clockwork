3eb1aec2ceaaf69e5989e7ac00b360fe5d9259de
Nick Garfield <nick@and.xyz>
2022-09-30 10:17:48 -0400

    Update SDK interfaces

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -6,12 +6,14 @@
 pub use clockwork_client::{Client, ClientError, ClientResult};
 
 #[cfg(feature = "client")]
-pub use clockwork_queue_program::{
-    anchor, errors, payer, queue_program, state, ID as QueueProgramId,
-};
+mod queue_program {
+    pub use clockwork_queue_program::{anchor, errors, payer, state, ID};
+}
 
 /**
  * Program level
  */
 #[cfg(not(feature = "client"))]
-pub use clockwork_queue_program::*;
+mod queue_program {
+    pub use clockwork_queue_program::*;
+}
