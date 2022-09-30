846f47ed6dd4ce733f5e57af8441263f2b4a1fd9
Nick Garfield <nick@and.xyz>
2022-09-29 23:08:54 -0400

    Adjust SDK export pattern

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,7 +1,16 @@
-pub use ::clockwork_queue_program::*;
+/**
+ * Client level
+ */
 
 #[cfg(feature = "client")]
 pub use clockwork_client::Client;
 
 #[cfg(feature = "client")]
 pub use clockwork_client::queue::*;
+
+/**
+ * Program level
+ */
+
+#[cfg(not(feature = "client"))]
+pub use clockwork_queue_program::*;
