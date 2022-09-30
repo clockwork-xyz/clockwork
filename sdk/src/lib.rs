61a20b79e529ecbc4a1be968091fc1ced8cef574
Nick Garfield <nick@and.xyz>
2022-09-30 11:34:29 -0400

    Cleanup export pattern to make features purely additive

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,22 +1,16 @@
 /**
- * Client level
- */
-
-#[cfg(feature = "client")]
-pub use clockwork_client::{Client, ClientError, ClientResult};
-
-#[cfg(feature = "client")]
-pub mod queue_program {
-    pub use clockwork_queue_program::{errors, state, utils, ID};
-}
-
-/**
  * Program level
  */
 
-#[cfg(not(feature = "client"))]
 pub mod queue_program {
     pub use clockwork_queue_program::{
         accounts, cpi, errors, program::QueueProgram, state, utils, ID,
     };
 }
+
+/**
+ * Client level
+ */
+
+#[cfg(feature = "client")]
+pub use clockwork_client::{Client, ClientError, ClientResult};
