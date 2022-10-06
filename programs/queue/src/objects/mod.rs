d77c13d48571c5f7272eeff64452ae3dc32f67ad
Nick Garfield <nick@and.xyz>
2022-10-05 22:11:30 -0500

    Delete clock from queue program objects mod

--- programs/queue/src/objects/mod.rs
+++ programs/queue/src/objects/mod.rs
@@ -1,11 +1,9 @@
 //! All objects needed to describe and manage the program's state.
 
-mod clock;
 mod config;
 mod fee;
 mod queue;
 
-pub use clock::*;
 pub use config::*;
 pub use fee::*;
 pub use queue::*;
