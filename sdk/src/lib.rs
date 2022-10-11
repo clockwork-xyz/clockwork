36f3fcc3eef95a03b7240cd6fcea0bbec31564e6
Nick Garfield <nick@and.xyz>
2022-10-11 10:55:05 -0500

    Remove nonexistent utils package from sdk

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -8,7 +8,7 @@ pub use clockwork_client::{Client, ClientError, ClientResult, SplToken};
 // For programs that need to CPI into Clockwork.
 #[cfg(feature = "queue")]
 pub mod queue_program {
-    pub use clockwork_queue_program::{cpi, errors, program::QueueProgram, utils, ID};
+    pub use clockwork_queue_program::{cpi, errors, program::QueueProgram, ID};
     pub mod accounts {
         pub use clockwork_queue_program::accounts::*;
         pub use clockwork_queue_program::objects::*;
