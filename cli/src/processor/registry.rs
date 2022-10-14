0cbbaa158bd525f2f7614b2592217706b016c756
Nick Garfield <nick@and.xyz>
2022-10-14 01:12:32 -0500

    Add snapshots, fees, and penalties to CLI output

--- cli/src/processor/registry.rs
+++ cli/src/processor/registry.rs
@@ -1,13 +1,24 @@
-use clockwork_client::network::objects::Registry;
-
-use {crate::errors::CliError, clockwork_client::Client};
+use {
+    crate::errors::CliError,
+    clockwork_client::{
+        network::objects::{Registry, Snapshot},
+        Client,
+    },
+};
 
 pub fn get(client: &Client) -> Result<(), CliError> {
     let registry_pubkey = clockwork_client::network::objects::Registry::pubkey();
     let registry = client
         .get::<Registry>(&registry_pubkey)
         .map_err(|_err| CliError::AccountDataNotParsable(registry_pubkey.to_string()))?;
+
+    let snapshot_pubkey = Snapshot::pubkey(registry.current_epoch);
+    let snapshot = client
+        .get::<Snapshot>(&snapshot_pubkey)
+        .map_err(|_err| CliError::AccountDataNotParsable(snapshot_pubkey.to_string()))?;
+
     println!("{:#?}", registry);
+    println!("{:#?}", snapshot);
     Ok(())
 }
 
--- cli/src/processor/worker.rs
+++ cli/src/processor/worker.rs
@@ -1,6 +1,8 @@
+use clockwork_client::network::objects::Penalty;
+
 use {
     crate::errors::CliError,
-    clockwork_client::network::objects::{Config, Registry, Worker},
+    clockwork_client::network::objects::{Config, Fee, Registry, Snapshot, SnapshotFrame, Worker},
     clockwork_client::Client,
     solana_sdk::signature::{Keypair, Signer},
 };
@@ -10,7 +12,54 @@ pub fn get(client: &Client, id: u64) -> Result<(), CliError> {
     let worker = client
         .get::<Worker>(&worker_pubkey)
         .map_err(|_err| CliError::AccountDataNotParsable(worker_pubkey.to_string()))?;
-    println!("Address:{}\n{:#?}", worker_pubkey, worker);
+
+    // Get fee balance
+    let fee_pubkey = Fee::pubkey(worker_pubkey);
+    let fee_data = client
+        .get_account_data(&fee_pubkey)
+        .map_err(|_err| CliError::AccountNotFound(fee_pubkey.to_string()))?;
+    let fees_min_rent = client
+        .get_minimum_balance_for_rent_exemption(fee_data.len())
+        .unwrap();
+    let fees_balance = client.get_balance(&fee_pubkey).unwrap();
+    let fees_total = fees_balance - fees_min_rent;
+
+    // Get penalty balance
+    let penalty_pubkey = Penalty::pubkey(worker_pubkey);
+    let penalty_data = client
+        .get_account_data(&penalty_pubkey)
+        .map_err(|_err| CliError::AccountNotFound(penalty_pubkey.to_string()))?;
+    let penalty_min_rent = client
+        .get_minimum_balance_for_rent_exemption(penalty_data.len())
+        .unwrap();
+    let penalty_balance = client.get_balance(&penalty_pubkey).unwrap();
+    let penalty_total = penalty_balance - penalty_min_rent;
+
+    println!(
+        "Address: {}\nFees: {}\nPenalty: {}\n{:#?}",
+        worker_pubkey, fees_total, penalty_total, worker
+    );
+
+    // Get registry
+    let registry_pubkey = Registry::pubkey();
+    let registry_data = client
+        .get_account_data(&registry_pubkey)
+        .map_err(|_err| CliError::AccountNotFound(registry_pubkey.to_string()))?;
+    let registry = Registry::try_from(registry_data)
+        .map_err(|_err| CliError::AccountDataNotParsable(registry_pubkey.to_string()))?;
+
+    // Get snapshot frame
+    let snapshot_pubkey = Snapshot::pubkey(registry.current_epoch);
+    let snapshot_frame_pubkey = SnapshotFrame::pubkey(snapshot_pubkey, worker.id);
+    match client.get_account_data(&snapshot_frame_pubkey) {
+        Err(_err) => {}
+        Ok(snapshot_frame_data) => {
+            let snapshot_frame = SnapshotFrame::try_from(snapshot_frame_data)
+                .map_err(|_err| CliError::AccountDataNotParsable(registry_pubkey.to_string()))?;
+            println!("{:#?}", snapshot_frame);
+        }
+    }
+
     Ok(())
 }
 
