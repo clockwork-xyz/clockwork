6ee9ac1545dcc64eb9f73046dbe2cf9aa5d6412e
Nick Garfield <nick@clockwork.xyz>
2022-12-02 16:03:17 -0600

    Refactor the SDK to remove the client dependency (#101)
    * Refactor the SDK to remove the client dependency
    
    * Refactor cpi interfaces
    
    * Upgrade Cargo.lock and resolve build errors
    
    * Fix solana program version to 1.13.4
    
    * Update lock file to fix build
    
    * Update the commit hash on the anchor fork branch

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,28 +1,70 @@
-// Utils
-pub use clockwork_thread_program::state::{
-    anchor_sighash, AccountMetaData, ClockData, InstructionData, ThreadResponse, PAYER_PUBKEY,
-};
+pub use clockwork_thread_program::errors;
 
-// For clients
-#[cfg(feature = "client")]
-pub mod client {
-    pub use clockwork_client::{
-        thread as thread_program, Client, ClientError, ClientResult, SplToken,
+pub use clockwork_thread_program::ID;
+
+pub mod state {
+    pub use clockwork_thread_program::state::{
+        AccountMetaData, ClockData, ExecContext, InstructionData, Thread, ThreadAccount,
+        ThreadResponse, ThreadSettings, Trigger, TriggerContext,
     };
 }
 
-// For programs that need to CPI into Clockwork.
-#[cfg(feature = "thread")]
-pub mod thread_program {
-    pub use clockwork_thread_program::{cpi, errors, program::ThreadProgram, ID};
-    pub mod accounts {
-        pub use clockwork_thread_program::state::{
-            ExecContext, Thread, ThreadAccount, ThreadSettings, Trigger, TriggerContext,
-        };
-    }
+pub mod utils {
+    pub use clockwork_thread_program::state::anchor_sighash;
+    pub use clockwork_thread_program::state::PAYER_PUBKEY;
 }
 
-#[cfg(feature = "utils")]
-pub mod utils {
-    pub use clockwork_utils::explorer;
+// #[cfg(feature = "cpi")]
+pub mod cpi {
+    use anchor_lang::prelude::{CpiContext, Result};
+
+    pub use clockwork_thread_program::cpi::accounts::{
+        ThreadCreate, ThreadDelete, ThreadPause, ThreadResume, ThreadStop, ThreadUpdate,
+        ThreadWithdraw,
+    };
+
+    pub fn thread_create<'info>(
+        ctx: CpiContext<'_, '_, '_, 'info, ThreadCreate<'info>>,
+        id: String,
+        kickoff_ix: crate::state::InstructionData,
+        trigger: crate::state::Trigger,
+    ) -> Result<()> {
+        clockwork_thread_program::cpi::thread_create(ctx, id, kickoff_ix, trigger)
+    }
+
+    pub fn thread_delete<'info>(
+        ctx: CpiContext<'_, '_, '_, 'info, ThreadDelete<'info>>,
+    ) -> Result<()> {
+        clockwork_thread_program::cpi::thread_delete(ctx)
+    }
+
+    pub fn thread_pause<'info>(
+        ctx: CpiContext<'_, '_, '_, 'info, ThreadPause<'info>>,
+    ) -> Result<()> {
+        clockwork_thread_program::cpi::thread_pause(ctx)
+    }
+
+    pub fn thread_resume<'info>(
+        ctx: CpiContext<'_, '_, '_, 'info, ThreadResume<'info>>,
+    ) -> Result<()> {
+        clockwork_thread_program::cpi::thread_resume(ctx)
+    }
+
+    pub fn thread_stop<'info>(ctx: CpiContext<'_, '_, '_, 'info, ThreadStop<'info>>) -> Result<()> {
+        clockwork_thread_program::cpi::thread_stop(ctx)
+    }
+
+    pub fn thread_update<'info>(
+        ctx: CpiContext<'_, '_, '_, 'info, ThreadUpdate<'info>>,
+        settings: crate::state::ThreadSettings,
+    ) -> Result<()> {
+        clockwork_thread_program::cpi::thread_update(ctx, settings)
+    }
+
+    pub fn thread_withdraw<'info>(
+        ctx: CpiContext<'_, '_, '_, 'info, ThreadWithdraw<'info>>,
+        amount: u64,
+    ) -> Result<()> {
+        clockwork_thread_program::cpi::thread_withdraw(ctx, amount)
+    }
 }
