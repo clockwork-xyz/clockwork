c20787ea8290cf3e9496aecbf8369f06d24d67ac
Nick Garfield <nick@clockwork.xyz>
2022-12-04 23:20:39 -0600

    Attempt to resolve build issue in cli

--- cli/build.rs
+++ cli/build.rs
@@ -3,29 +3,31 @@ use regex::Regex;
 use std::process::Command;
 
 fn main() {
-    let validator_version = get_validator_version();
+    let validator_version = get_validator_version().ok();
     let geyser_interface_version = get_geyser_interface_version();
 
-    println!("cargo:rustc-env=VALIDATOR_VERSION={}", validator_version);
+    println!("cargo:rustc-env=VALIDATOR_VERSION={:#?}", validator_version);
     println!(
         "cargo:rustc-env=GEYSER_INTERFACE_VERSION={}",
         geyser_interface_version
     );
 }
 
-fn get_validator_version() -> String {
-    let output = Command::new("solana-test-validator")
+fn get_validator_version() -> Result<String, std::io::Error> {
+    Command::new("solana-test-validator")
         .arg("--version")
         .output()
-        .unwrap();
-    let version = String::from_utf8_lossy(&output.stdout);
-    let re = Regex::new(r"(\d{1}\.\d{2}\.\d{1})").unwrap();
-    let caps = re.captures(&version).unwrap();
-    caps.get(1)
-        .map_or("unknown (error parsing solana-validator version)", |m| {
-            m.as_str()
+        .and_then(|output| {
+            let version = String::from_utf8_lossy(&output.stdout);
+            let re = Regex::new(r"(\d{1}\.\d{2}\.\d{1})").unwrap();
+            let caps = re.captures(&version).unwrap();
+            Ok(caps
+                .get(1)
+                .map_or("unknown (error parsing solana-validator version)", |m| {
+                    m.as_str()
+                })
+                .into())
         })
-        .into()
 }
 
 fn get_geyser_interface_version() -> String {
