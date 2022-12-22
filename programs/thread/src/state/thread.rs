a7237d114f35bfe3bfbe8379379bcc85cd4b7389
Nick Garfield <nick@clockwork.xyz>
2022-12-21 18:11:46 -0600

    Refactor kickoff logic into instruction file

--- programs/thread/src/state/thread.rs
+++ programs/thread/src/state/thread.rs
@@ -9,15 +9,8 @@ use {
         },
         AnchorDeserialize, AnchorSerialize,
     },
-    chrono::{DateTime, NaiveDateTime, Utc},
-    clockwork_cron::Schedule,
     clockwork_network_program::state::{Fee, Penalty, Pool, Worker},
-    std::{
-        collections::hash_map::DefaultHasher,
-        convert::TryFrom,
-        hash::{Hash, Hasher},
-        str::FromStr,
-    },
+    std::convert::TryFrom,
 };
 
 pub const SEED_THREAD: &[u8] = b"thread";
@@ -78,13 +71,6 @@ impl TryFrom<Vec<u8>> for Thread {
     }
 }
 
-impl Hash for Thread {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.authority.hash(state);
-        self.id.hash(state);
-    }
-}
-
 impl PartialEq for Thread {
     fn eq(&self, other: &Self) -> bool {
         self.authority.eq(&other.authority) && self.id.eq(&other.id)
@@ -127,8 +113,6 @@ pub trait ThreadAccount {
         signatory: &mut Signer,
         worker: &Account<Worker>,
     ) -> Result<()>;
-
-    fn kickoff(&mut self, remaining_accounts: &[AccountInfo]) -> Result<()>;
 
     /// Reallocate the memory allocation for the account.
     fn realloc(&mut self) -> Result<()>;
@@ -367,123 +351,6 @@ impl ThreadAccount for Account<'_, Thread> {
 
         Ok(())
     }
-
-    fn kickoff(&mut self, remaining_accounts: &[AccountInfo]) -> Result<()> {
-        let clock = Clock::get().unwrap();
-        match self.trigger.clone() {
-            Trigger::Account {
-                address,
-                offset,
-                size,
-            } => {
-                // Verify proof that account data has been updated.
-                match remaining_accounts.first() {
-                    None => {}
-                    Some(account_info) => {
-                        // Verify the remaining account is the account this thread is listening for.
-                        require!(
-                            address.eq(account_info.key),
-                            ClockworkError::TriggerNotActive
-                        );
-
-                        // Begin computing the data hash of this account.
-                        let mut hasher = DefaultHasher::new();
-                        let data = &account_info.try_borrow_data().unwrap();
-                        let range_end = offset.checked_add(size).unwrap();
-                        if data.len().gt(&range_end) {
-                            data[offset..range_end].hash(&mut hasher);
-                        } else {
-                            data[offset..].hash(&mut hasher)
-                        }
-                        let data_hash = hasher.finish();
-
-                        // Verify the data hash is different than the prior data hash.
-                        if let Some(exec_context) = self.exec_context {
-                            match exec_context.trigger_context {
-                                TriggerContext::Account {
-                                    data_hash: prior_data_hash,
-                                } => {
-                                    require!(
-                                        data_hash.ne(&prior_data_hash),
-                                        ClockworkError::TriggerNotActive
-                                    )
-                                }
-                                _ => return Err(ClockworkError::InvalidThreadState.into()),
-                            }
-                        }
-
-                        // Set a new exec context with the new data hash and slot number.
-                        self.exec_context = Some(ExecContext {
-                            execs_since_reimbursement: 0,
-                            execs_since_slot: 0,
-                            last_exec_at: clock.slot,
-                            trigger_context: TriggerContext::Account { data_hash },
-                        })
-                    }
-                }
-            }
-            Trigger::Cron {
-                schedule,
-                skippable,
-            } => {
-                // Get the reference timestamp for calculating the thread's scheduled target timestamp.
-                let reference_timestamp = match self.exec_context.clone() {
-                    None => self.created_at.unix_timestamp,
-                    Some(exec_context) => match exec_context.trigger_context {
-                        TriggerContext::Cron { started_at } => started_at,
-                        _ => return Err(ClockworkError::InvalidThreadState.into()),
-                    },
-                };
-
-                // Verify the current timestamp is greater than or equal to the threshold timestamp.
-                let threshold_timestamp = next_timestamp(reference_timestamp, schedule.clone())
-                    .ok_or(ClockworkError::TriggerNotActive)?;
-                require!(
-                    clock.unix_timestamp.ge(&threshold_timestamp),
-                    ClockworkError::TriggerNotActive
-                );
-
-                // If the schedule is marked as skippable, set the started_at of the exec context to be the current timestamp.
-                // Otherwise, the exec context must iterate through each scheduled kickoff moment.
-                let started_at = if skippable {
-                    clock.unix_timestamp
-                } else {
-                    threshold_timestamp
-                };
-
-                // Set the exec context.
-                self.exec_context = Some(ExecContext {
-                    execs_since_reimbursement: 0,
-                    execs_since_slot: 0,
-                    last_exec_at: clock.slot,
-                    trigger_context: TriggerContext::Cron { started_at },
-                });
-            }
-            Trigger::Immediate => {
-                // Set the exec context.
-                require!(
-                    self.exec_context.is_none(),
-                    ClockworkError::InvalidThreadState
-                );
-                self.exec_context = Some(ExecContext {
-                    execs_since_reimbursement: 0,
-                    execs_since_slot: 0,
-                    last_exec_at: clock.slot,
-                    trigger_context: TriggerContext::Immediate,
-                });
-            }
-        }
-
-        // If we make it here, the trigger is active. Update the next instruction and be done.
-        if let Some(kickoff_instruction) = self.instructions.first() {
-            self.next_instruction = Some(kickoff_instruction.clone());
-        }
-
-        // Realloc the thread account
-        self.realloc()?;
-
-        Ok(())
-    }
 }
 
 /// The triggering conditions of a thread.
@@ -514,7 +381,7 @@ pub enum Trigger {
 }
 
 /// The execution context of a particular transaction thread.
-#[derive(AnchorDeserialize, AnchorSerialize, Clone, Copy, Debug, Hash, PartialEq, Eq)]
+#[derive(AnchorDeserialize, AnchorSerialize, Clone, Copy, Debug, PartialEq, Eq)]
 pub struct ExecContext {
     /// Number of execs since the last tx reimbursement.
     pub execs_since_reimbursement: u64,
@@ -530,7 +397,7 @@ pub struct ExecContext {
 }
 
 /// The event which allowed a particular transaction thread to be triggered.
-#[derive(AnchorDeserialize, AnchorSerialize, Clone, Copy, Debug, Hash, PartialEq, Eq)]
+#[derive(AnchorDeserialize, AnchorSerialize, Clone, Copy, Debug, PartialEq, Eq)]
 pub enum TriggerContext {
     /// A running hash of the observed account data.
     Account {
@@ -546,15 +413,4 @@ pub enum TriggerContext {
 
     /// The immediate trigger context.
     Immediate,
-}
-
-fn next_timestamp(after: i64, schedule: String) -> Option<i64> {
-    Schedule::from_str(&schedule)
-        .unwrap()
-        .next_after(&DateTime::<Utc>::from_utc(
-            NaiveDateTime::from_timestamp(after, 0),
-            Utc,
-        ))
-        .take()
-        .map(|datetime| datetime.timestamp())
 }
