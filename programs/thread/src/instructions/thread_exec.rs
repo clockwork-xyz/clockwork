88f5d2a2aa8c3f1911d4def17e613a23798e8a61
Nick Garfield <nick@clockwork.xyz>
2023-01-20 17:11:00 +0000

    Use saturating_sub for code compactness

--- programs/thread/src/instructions/thread_exec.rs
+++ programs/thread/src/instructions/thread_exec.rs
@@ -192,13 +192,7 @@ pub fn handler(ctx: Context<ThreadExec>) -> Result<()> {
 
     // Reimbursement signatory for lamports paid during inner ix.
     let signatory_lamports_post = signatory.lamports();
-    let signatory_reimbursement = if signatory_lamports_pre.gt(&signatory_lamports_post) {
-        signatory_lamports_pre
-            .checked_sub(signatory_lamports_post)
-            .unwrap()
-    } else {
-        0
-    };
+    let signatory_reimbursement = signatory_lamports_pre.saturating_sub(signatory_lamports_post);
     if signatory_reimbursement.gt(&0) {
         **thread.to_account_info().try_borrow_mut_lamports()? = thread
             .to_account_info()
