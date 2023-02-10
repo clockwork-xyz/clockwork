02ad0915a627c7424de876ed06dcaddc84260ced
Nick Garfield <nick@clockwork.xyz>
2023-02-10 23:38:58 +0000

    Re-enable account_info parsing

--- plugin/src/plugin.rs
+++ plugin/src/plugin.rs
@@ -67,26 +67,26 @@ impl GeyserPlugin for ClockworkPlugin {
         is_startup: bool,
     ) -> PluginResult<()> {
         // Parse account info.
-        // let account_info = &mut match account {
-        //     ReplicaAccountInfoVersions::V0_0_1(account_info) => ReplicaAccountInfo {
-        //         pubkey: account_info.pubkey,
-        //         lamports: account_info.lamports,
-        //         owner: account_info.owner,
-        //         executable: account_info.executable,
-        //         rent_epoch: account_info.rent_epoch,
-        //         data: account_info.data,
-        //         write_version: account_info.write_version,
-        //     },
-        //     ReplicaAccountInfoVersions::V0_0_2(account_info) => ReplicaAccountInfo {
-        //         pubkey: account_info.pubkey,
-        //         lamports: account_info.lamports,
-        //         owner: account_info.owner,
-        //         executable: account_info.executable,
-        //         rent_epoch: account_info.rent_epoch,
-        //         data: account_info.data,
-        //         write_version: account_info.write_version,
-        //     },
-        // };
+        let account_info = &mut match account {
+            ReplicaAccountInfoVersions::V0_0_1(account_info) => ReplicaAccountInfo {
+                pubkey: account_info.pubkey,
+                lamports: account_info.lamports,
+                owner: account_info.owner,
+                executable: account_info.executable,
+                rent_epoch: account_info.rent_epoch,
+                data: account_info.data,
+                write_version: account_info.write_version,
+            },
+            ReplicaAccountInfoVersions::V0_0_2(account_info) => ReplicaAccountInfo {
+                pubkey: account_info.pubkey,
+                lamports: account_info.lamports,
+                owner: account_info.owner,
+                executable: account_info.executable,
+                rent_epoch: account_info.rent_epoch,
+                data: account_info.data,
+                write_version: account_info.write_version,
+            },
+        };
         // let account_pubkey = Pubkey::new(account_info.pubkey);
         // let event = AccountUpdateEvent::try_from(account_info);
 
