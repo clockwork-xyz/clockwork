6f18165afda0749ef5da67279d9d7eac926f5fa3
Nick Garfield <nick@clockwork.xyz>
2023-02-16 05:05:26 +0000

    Top up thread if new instruction pushes it above min rent requirement

--- programs/thread/src/instructions/thread_instruction_add.rs
+++ programs/thread/src/instructions/thread_instruction_add.rs
@@ -1,12 +1,22 @@
-use {crate::state::*, anchor_lang::prelude::*};
+use anchor_lang::{
+    prelude::*,
+    solana_program::system_program,
+    system_program::{transfer, Transfer},
+};
+
+use crate::state::*;
 
 /// Accounts required by the `thread_instruction_add` instruction.
 #[derive(Accounts)]
 #[instruction(instruction: SerializableInstruction)]
 pub struct ThreadInstructionAdd<'info> {
     /// The authority (owner) of the thread.
-    #[account()]
+    #[account(mut)]
     pub authority: Signer<'info>,
+
+    /// The Solana system program
+    #[account(address = system_program::ID)]
+    pub system_program: Program<'info, System>,
 
     /// The thread to be paused.
     #[account(
@@ -27,12 +37,33 @@ pub fn handler(
     instruction: SerializableInstruction,
 ) -> Result<()> {
     // Get accounts
+    let authority = &ctx.accounts.authority;
     let thread = &mut ctx.accounts.thread;
+    let system_program = &ctx.accounts.system_program;
 
     // Append the instruction.
     thread.instructions.push(instruction);
 
-    // TODO Realloc account space.
+    // Reallocate mem for the thread account.
+    thread.realloc()?;
+
+    // If lamports are required to maintain rent-exemption, pay them.
+    let data_len = 8 + thread.try_to_vec()?.len();
+    let minimum_rent = Rent::get().unwrap().minimum_balance(data_len);
+    if minimum_rent > thread.to_account_info().lamports() {
+        transfer(
+            CpiContext::new(
+                system_program.to_account_info(),
+                Transfer {
+                    from: authority.to_account_info(),
+                    to: thread.to_account_info(),
+                },
+            ),
+            minimum_rent
+                .checked_sub(thread.to_account_info().lamports())
+                .unwrap(),
+        )?;
+    }
 
     Ok(())
 }
