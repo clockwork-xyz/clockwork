a1658ffc31257438d66da143c77ffe08dad7868f
Nick Garfield <nick@clockwork.xyz>
2023-02-21 16:06:31 +0000

    Print slot and blockhash

--- src/components/clock.rs
+++ src/components/clock.rs
@@ -1,24 +1,22 @@
 use chrono::Utc;
 use dioxus::prelude::*;
-use solana_client_wasm::{solana_sdk::commitment_config::CommitmentConfig, WasmClient};
+use solana_client_wasm::WasmClient;
 
 pub fn Clock(cx: Scope) -> Element {
+    let blockhash = use_state(&cx, || String::new());
     let slot = use_state(&cx, || 0);
     let time = use_state(&cx, || Utc::now());
 
     use_future(&cx, (), |_| {
+        let blockhash = blockhash.clone();
         let slot = slot.clone();
         let time = time.clone();
-        let client = WasmClient::new("https://api.devnet.solana.com");
+        let client = WasmClient::new("http://74.118.139.244:8899");
         async move {
             loop {
+                blockhash.set(client.get_latest_blockhash().await.unwrap().to_string());
                 time.set(Utc::now());
-                slot.set(
-                    client
-                        .get_slot_with_commitment(CommitmentConfig::processed())
-                        .await
-                        .unwrap(),
-                );
+                slot.set(client.get_slot().await.unwrap());
                 gloo_timers::future::TimeoutFuture::new(1000).await;
             }
         }
@@ -27,7 +25,7 @@ pub fn Clock(cx: Scope) -> Element {
     cx.render(rsx! {
         p {
             class: "fixed bottom-0 right-0 p-4",
-            format!("Slot: {} {}", slot, time.to_rfc3339())
+            format!("Blockhash: {} Slot: {} {}", blockhash, slot, time.to_rfc3339())
         }
     })
 }
