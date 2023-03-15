a4daab8389afa64e5c3f7c9900acd8689ad74b01
Eliascm17 <Elias.cmoreno17@gmail.com>
2023-03-14 23:03:32 -0500

    integrating VersionedThread and added more helpful error logs

--- app/src/pages/thread.rs
+++ app/src/pages/thread.rs
@@ -1,20 +1,19 @@
 use std::str::FromStr;
-
 use std::convert::From;
-use super::Page;
-use crate::clockwork::{get_thread, simulate_thread};
-use anchor_lang::solana_program::{pubkey::Pubkey, instruction::Instruction};
-use clockwork_sdk::state::Thread;
+use anchor_lang::solana_program::{pubkey::Pubkey, instruction::InstructionError}; 
 use dioxus::prelude::*;
 use dioxus_router::use_route;
-use dotenv_codegen::dotenv;
-use solana_client_wasm::{WasmClient, solana_sdk::{compute_budget::{self, ComputeBudgetInstruction}, transaction::TransactionError}};
+use clockwork_thread_program_v2::state::VersionedThread;
+use solana_client_wasm::solana_sdk::transaction::TransactionError;
+
+use crate::clockwork::{get_thread, simulate_thread};
+
+use super::Page;
 
 pub fn ThreadPage(cx: Scope) -> Element {
     let route = use_route(cx);
-    let thread = use_state::<Option<Thread>>(cx, || None);
+    let thread = use_state::<Option<VersionedThread>>(cx, || None);
     let thread_pubkey = Pubkey::from_str(route.last_segment().unwrap()).unwrap();
-    // let show_modal = use_state::<bool>(cx, || false);
     let logs = use_state::<Vec<String>>(cx, || vec![]);
     let log_errors = use_state::<Option<TransactionError>>(cx, || None);
 
@@ -26,7 +25,7 @@ pub fn ThreadPage(cx: Scope) -> Element {
         async move { 
             let t = get_thread(thread_pubkey).await;
                 thread.set(Some(t.clone())) ;
-                    match simulate_thread(t.to_owned()).await {
+                    match simulate_thread(t.to_owned(), thread_pubkey).await {
                         Ok(res) => {
                             logs.set(res.1.unwrap());                           
                             log_errors.set(res.0);                                
@@ -36,27 +35,33 @@ pub fn ThreadPage(cx: Scope) -> Element {
         }
     });
 
-    // let handle_click = move |_| {
-    //     cx.spawn({
-    //         let thread = thread.clone();
-    //         // let show_modal = show_modal.clone();
-    //         let logs = logs.clone();
-    //         let log_errors = log_errors.clone();
-    //         // show_modal.set(true);
-    //         async move {
-    //             if let Some(t) = thread.get() {
-    //                 match simulate_thread(t.to_owned()).await {
-    //                     Ok(res) => {
-    //                         logs.set(res.1.unwrap());                           
-    //                         log_errors.set(res.0);                                
-    //                     },
-    //                     Err(_err) => {}
-    //                 }
-    //             }
-    //         }
-    //     });
-    // };
+    let tx_err = if log_errors.get().is_some() {
+        let mut hex: Option<String> = None;
+        let raw_text = log_errors.as_ref().unwrap().to_string();
+        let mut error_string = String::new();
+        for word in raw_text.split(" ").collect::<Vec<_>>().iter() {
+            hex = match word.strip_prefix("0x") {
+                Some(d) => Some(String::from(d.to_string())),
+                None => {
+                    let w = String::from(format!("{} ", word));
+                    error_string.push_str(w.as_str());
+                    None
+                },
+            }
+        }
 
+        if hex.is_some() {
+            let dec = i64::from_str_radix(hex.unwrap().as_str(), 16);
+            String::from(format!("{}{}", error_string, dec.unwrap()))
+        } else {
+            String::from(format!("{}", error_string))
+        }
+        
+    } else {
+        String::from("")
+    };
+
+    
     if let Some(t) = thread.get() {
         cx.render(rsx! {
             Page {
@@ -76,20 +81,16 @@ pub fn ThreadPage(cx: Scope) -> Element {
                                     value: thread_pubkey.to_string()
                                 }
                                 Row {
+                                    label: "Thread Program".to_string(),
+                                    value: t.program_id().to_string(),
+                                }
+                                Row {
                                     label: "Authority".to_string(),
-                                    value: t.authority.to_string(),
-                                }
-                                Row {
-                                    label: "Fee".to_string(),
-                                    value: t.fee.to_string(),
-                                }
-                                Row {
-                                    label: "ID".to_string(),
-                                    value: t.id.to_string(),
+                                    value: t.authority().to_string(),
                                 }
                                 Row {
                                     label: "Paused".to_string(),
-                                    value: t.paused.to_string(),
+                                    value: t.paused().to_string(),
                                 }
                             }
                         }
@@ -101,7 +102,11 @@ pub fn ThreadPage(cx: Scope) -> Element {
                             "Simulation Logs"
                         }
                         code {
-                            class: "w-full h-auto flex flex-col px-4 py-4 font-mono text-base text-slate-100 break-all",
+                            class: "w-full h-auto flex flex-col px-4 py-4 mt-2 font-mono text-base text-slate-100 break-all rounded-md bg-red-500",
+                            "{tx_err}"
+                        }
+                        code {
+                            class: "w-full h-auto flex flex-col px-4 py-2 font-mono text-base text-slate-100 break-all",
                             for log in logs.get().iter() {
                               p {
                                     "{log}"
