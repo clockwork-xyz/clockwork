3fb5c9dc017e2c757dbcc6acf26e673711397e17
Eliascm17 <Elias.cmoreno17@gmail.com>
2023-03-30 12:27:34 -0500

    Add ability to filter Threads by `authority` (#214)
    * move hook to be an inline prop and update logic from hook rewrite
    
    * allow for simple filtering by thread authority pubkey (user logged in)
    
    * added helius devnet rpc url to avoid rate limiting
    
    * rewrite use_pagination hook using use_ref
    
    * added uuid for unique IDs for each use_pagination hook instance

--- app/src/components/page_control.rs
+++ app/src/components/page_control.rs
@@ -2,38 +2,25 @@ use dioxus::prelude::*;
 
 use crate::hooks::UsePagination;
 
-#[derive(Clone, Props)]
-pub struct PaginationControlsProps<'a, T: Clone + 'static> {
-    pub paginated_data: &'a UsePagination<T>,
-}
-
-impl<'a, T: Clone + 'static> PartialEq for PaginationControlsProps<'a, T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.paginated_data
-            .table_id
-            .eq(&other.paginated_data.table_id)
-    }
-}
-
-pub fn PageControl<'a, T: Clone + 'static>(
-    cx: Scope<'a, PaginationControlsProps<'a, T>>,
-) -> Element<'a> {
-    let paginated_data = &cx.props.paginated_data;
+#[inline_props]
+pub fn PageControl<T: Clone + 'static>(cx: Scope, paginated_data: UsePagination<T>) -> Element {
     let button_class = "py-2 px-6 text-slate-100 hover:bg-slate-800 active:bg-slate-100 active:text-slate-900 transition text-sm font-medium rounded";
     let hidden_button_class = "py-2 px-6 text-slate-100 hover:bg-slate-800 active:bg-slate-100 active:text-slate-900 transition text-sm font-medium rounded invisible";
-    let back_button_class = if paginated_data.current_page().gt(&0) {
+    let back_button_class = if paginated_data.current_page().gt(&1) {
         button_class
     } else {
         hidden_button_class
     };
+
     let forward_button_class = if paginated_data
         .current_page()
-        .lt(&(paginated_data.total_pages() - 1))
+        .eq(&paginated_data.total_pages())
     {
-        button_class
-    } else {
         hidden_button_class
+    } else {
+        button_class
     };
+
     cx.render(rsx! {
         div {
             class: "flex items-center justify-center space-x-4 mt-2",
@@ -44,7 +31,7 @@ pub fn PageControl<'a, T: Clone + 'static>(
             }
             div {
                 class: "text-sm text-slate-100",
-                "{paginated_data.current_page() + 1} of {paginated_data.total_pages()}"
+                "{paginated_data.current_page()} of {paginated_data.total_pages()}"
             }
             button {
                 class: forward_button_class,
