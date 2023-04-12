f0737972cb960df068f784f02cb4939fb7ea4f53
mwrites <20499416+mwrites@users.noreply.github.com>
2023-04-13 03:57:19 +0700

    not needed

--- app/build.sh
+++ app/build.sh
@@ -12,9 +12,6 @@ export PATH="$CARGO_HOME/bin:$PATH"
 
 # Install trunk
 cargo install trunk
-set +e
-cargo update -p time:0.3.20 --precise 0.3.16
-set -e
 
 # Update wasm target
 rustup target add wasm32-unknown-unknown
