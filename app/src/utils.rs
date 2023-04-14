f29d832fb3b770a54273d18c48801ae5fb6aa276
Eliascm17 <Elias.cmoreno17@gmail.com>
2023-04-14 15:07:59 -0500

    add utils functinos to parse url

--- app/src/utils.rs
+++ app/src/utils.rs
@@ -1,5 +1,6 @@
 use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 use chrono::{DateTime, NaiveDateTime, Utc};
+use url::Url;
 
 pub fn format_balance(lamports: u64, short: bool) -> String {
     let balance = lamports as f64 / LAMPORTS_PER_SOL as f64;
@@ -16,4 +17,28 @@ pub fn format_timestamp(timestamp: i64) -> String {
         Utc,
     );
     format!("{}", dt.format("%F %H:%M UTC"))
+}
+
+pub fn parse_url(url_str: &str) -> Result<(String, String, String), url::ParseError> {
+    let url = Url::parse(url_str)?;
+
+    let domain = format!(
+        "{}://{}{}",
+        url.scheme(),
+        url.host_str().unwrap_or(""),
+        url.port()
+            .map(|p| format!(":{}", p))
+            .unwrap_or_else(|| "".to_string())
+    );
+    let route = url.path().to_string();
+    let query = url.query().unwrap_or("").to_string();
+
+    Ok((domain, route, query))
+}
+
+pub fn current_route(url_str: &str) -> Result<String, url::ParseError> {
+    let url = Url::parse(url_str)?;
+    let route = url.path().to_string();
+
+    Ok(route)
 }
